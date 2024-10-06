d9191a6bee9ba01400093f74cdf8ec95e8cc6cb1
Yureka-Adhikari <adhikariyureka@gmail.com>
2024-10-06 20:38:17 +0545

    Quiz Project-2

---
+++ Project 2.py
@@ -0,0 +1,246 @@
+print("Welcome to an interactive Quiz!")
+print("There are 30 questions, each question ears you one point except a couple with 2 (will be mentioned in the question)")
+score= 0
+print("Question no.1")
+
+question_1= input("What is the only prime number which is also even?")
+if question_1== "2":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 2")
+    
+question_2= input("What is the square of 11?")
+if question_2== "121":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 121")
+
+question_3= input("True or false: The answer to 56 + 89 x 2 is 978")
+if question_3== "False":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was False since the answer is 235 not 978")
+    
+question_4= input ("What is the double of 6")
+if question_4 == "12":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 12")
+question_5= input("1000 raised to the power 0 is equal to?")
+if question_5== "1":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 1")
+
+question_6= input("-10 is a whole number. True or false?")
+if question_6== "False":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was False")
+
+question_7= input("The largest 4 digit number is 9999, True or false?")
+if question_7== "True":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was True")
+
+question_8= input(" Cube of 5 is equal to?")
+if question_8== "125":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 125")
+
+question_9= input("How many sides does a decagon have? (in digit)")
+if question_9== "10":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 10")
+
+question_10= input("-5 an integer? Yes or No?")
+if question_10== "Yes":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was Yes")
+
+question_11= input("Is triangle a two-dimensional or three-dimensional shape? Yes or no?")
+if question_11== "Yes":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was Yes")
+
+question_12= input("An equilateral triangle has two of its sides equal. True or false?")
+if question_12== "True":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was True")
+
+question_13= input("27 is a perfect cube. True or False?")
+if question_13== "True":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was True")
+
+question_14= input("What is the formula for area of rectangle?")
+if question_14== "l x b":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was l x b")
+
+question_15= input("A square has all its angles equal to what degree (only the integer)")
+if question_15== "90":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was False")
+
+
+question_16= input("What is the only prime number which is also even?")
+if question_16== "2":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 2")
+    
+    question_17= input("What is the square of 11?")
+if question_17== "121":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 121")
+
+question_18= input("True or false: The answer to 89 x 78 is 6945")
+if question_18== "False":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was False since the answer is 235 not 978")
+
+question_19= input("What is the next prime number after 5??")
+if question_19== "7":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 7")
+
+question_20= input("The circumference of the circle is also called what?")
+if question_20== "perimeter of the circle":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 1")
+
+question_21= input("0 is a whole number. True or false?")
+if question_21== "True":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was True")
+
+question_22= input("The smallest 7 digit number is 1890, True or false?")
+if question_22== "False":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was False")
+
+question_23= input(" Square of 9 is equal to?")
+if question_23== "81":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 81")
+
+question_24= input("What is the only number that is neither prime nor composite?")
+if question_24== "1":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 1")
+
+question_25= input("A rational number can be represented in a/b format where b is not equal to 0. Yes or No?")
+if question_25== "Yes":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was Yes")
+
+question_26= input("Is cube a two-dimensional or three-dimensional shape? Yes or no?")
+if question_26== "Np":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was No")
+
+question_27= input("In a rectangle parallel sides are equal. True or false?")
+if question_27== "True":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was True")
+
+question_28= input("64 is a perfect cube. True or False?")
+if question_28== "False":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was False")
+
+question_29= input("What is the formula for the perimeter of a square??")
+if question_29== "4l":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was 4l")
+
+question_30= input("Are all sides of a square equal? Yes or no?")
+if question_30== "Yes":
+    print("Correct!")
+    score= score + 1
+else:
+    print("Wrong!")
+    print("Correct answer was No")
+
+
